283a8d771387a25b6d2ff3cdb37cb46688c00b1f
cavem <kyle.stinar@hotmail.com>
2024-07-08 08:39:11 -0700

    python changes

--- 1main/Python/sn_utils_parse/script.py
+++ 1main/Python/sn_utils_parse/script.py
@@ -8,6 +8,7 @@ import os
 
 convert = {
     " ":  "%20",
+    # "   ":  "%20%20%20%20",
     "=":  "%3D",
     "(":  "%28",
     '"':  "%22",
@@ -33,26 +34,36 @@ convert = {
 
 url = "/sys.scripts.do?content="
 
+path = "1main/Python/sn_utils_parse/"
+
 print("--------------start----------------------")
 print()
-dn = os.path.abspath(".1general/notes/SNOW/sn_utils_parse/script.js")
+dn = os.path.abspath(path + "script.js")
 
 with open(dn, 'r') as f2:
     lines = f2.readlines()
     for line in lines:
+        # print("'" + line + "'")
+        # if line == " " or line == "\n":
+        #     continue
         for item in line:
             if item in convert:
-                url += convert[item]
+                data = convert[item]
             else:
-                url += item
-        # url += "%0A"
+                data = item
 
-dn = os.path.abspath(".1general/notes/SNOW/sn_utils_parse/string.txt")
+            # print("'" + data + "'" + "-", data.isspace())
+            if data.isspace():
+                url += "%20%20%20%20"
+            else:
+                url += data
+
+
+dn = os.path.abspath(path + "string.txt")
 
 with open(dn, 'w') as f3:
     f3.write(url)
 
-print(url)
+# print(url)
 
-print()
 print("-------------end---------------")
